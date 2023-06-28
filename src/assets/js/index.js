ee1a23e47878f03b5891130315f35ae2c18fb836
PrisonRP <136730982+PrisonRP@users.noreply.github.com>
2023-06-28 18:17:13 +0200

    Edit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Ta les cramptés ?", "author": "Volkar" },
+            { "message": "Mhhhh ?", "author": "Meek" },
+            { "message": "Vous avez de l'eau vous ?", "author": "Adama" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
